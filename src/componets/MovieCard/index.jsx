f297fb5e0a6be06ad8d05ec86d513240464a927a
Fernando <fernando.bragga@gmail.com>
2020-11-23 22:59:03 +1100

    add fixed rating

--- src/componets/MovieCard/index.jsx
+++ src/componets/MovieCard/index.jsx
@@ -13,27 +13,40 @@ import StarIcon from "@material-ui/icons/Star";
 import TrackChangesIcon from "@material-ui/icons/TrackChanges";
 
 const MovieCard = (props) => {
-  const { moviePoster, movieTitle, movieYear, movieImdbID } = props;
+  // debugger;
+
+  const handleOnRatingClick = props.onRatingClick;
+  const { Poster, Title, Year, imdbID, rating = 0 } = props.movie;
+  // const rating = props.movie || null;
+  const posterPlaceholder =
+    "https://i2.wp.com/www.theatrecr.org/wp-content/uploads/2016/01/poster-placeholder.png?ssl=1";
 
   return (
     <div style={{ maxWidth: 250 }}>
       <Card>
         <CardHeader
-          avatar={<Avatar>T</Avatar>}
-          title={movieTitle}
-          subheader={movieYear}
+          style={{ maxHeight: 72 }}
+          avatar={<Avatar>{Title[0]}</Avatar>}
+          title={Title}
+          subheader={Year}
         />
         <CardMedia
           component="img"
-          image={moviePoster}
-          title={movieTitle}
+          image={Poster === "N/A" ? posterPlaceholder : Poster}
+          title={Title}
           style={{ maxHeight: 300 }}
         />
         <CardContent>
-          <Typography>{movieImdbID}</Typography>
+          <Typography>{imdbID}</Typography>
+          <Typography>Rating: {rating}</Typography>
         </CardContent>
         <CardActions style={{ display: "flex", justifyContent: "space-between" }}>
-          <Button size="small" color="primary" startIcon={<TrackChangesIcon />}>
+          <Button
+            size="small"
+            color="primary"
+            startIcon={<TrackChangesIcon />}
+            onClick={() => handleOnRatingClick(imdbID)}
+          >
             RATING
           </Button>
 
--- src/componets/ResultArea/index.jsx
+++ src/componets/ResultArea/index.jsx
@@ -4,7 +4,41 @@ import MovieCard from "../MovieCard";
 
 const ResultArea = (props) => {
   const queryResults = props.queryResults;
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+
+  const handleOnRatingClick = (movieID) => {
+    const movieIndex = movies.findIndex((movie) => {
+      return movie.imdbID === movieID;
+    });
+
+    if (movieIndex >= 0) {
+      // const updatedMovie = movies[movieIndex];
+      // updatedMovie.rating = 10;
+      // console.log(updatedMovie);
+      const updatedMovie = movies[movieIndex];
+      updatedMovie.rating = 10;
+      const updatedMovies = [...movies];
+      updatedMovies[movieIndex] = updatedMovie;
+      setMovies(updatedMovies);
+      // setMovies(updatedMovies);
+      // setMovies([...movies, ])
+    }
+
+    // console.log(movieIndex);
+    // const updatedMovieRating = movies.find((movie) => {
+    //   return movie.imdbID === movieID;
+    // });
+    // if (updatedMovieRating) {
+    //   updatedMovieRating.rating = 10;
+    //   setMovies([...movies, {updatedMovieRating }]);
+    // }
+
+    // movies.map(movie => {
+    //   if (movie.imdbID === movieID){
+    //     setMovies([...movie])
+    //   }
+    // })
+  };
 
   useEffect(() => {
     if (queryResults) {
@@ -15,18 +49,12 @@ const ResultArea = (props) => {
   return (
     <div style={{ marginTop: 99 }}>
       <Grid container justify="center" spacing={5}>
-        {!movies ? (
+        {movies.length === 0 ? (
           <Typography>Your movie list is empty. Try searching one...</Typography>
         ) : (
           movies.map((m) => (
-            <Grid item>
-              <MovieCard
-                key={m.imdbID}
-                moviePoster={m.Poster}
-                movieTitle={m.Title}
-                movieYear={m.Year}
-                movieImdbID={m.imdbID}
-              />
+            <Grid item key={m.imdbID}>
+              <MovieCard movie={m} onRatingClick={handleOnRatingClick} />
             </Grid>
           ))
         )}
